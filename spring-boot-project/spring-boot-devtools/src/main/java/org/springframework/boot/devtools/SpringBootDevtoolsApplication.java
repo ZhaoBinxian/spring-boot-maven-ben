11394a93853c139216a91e00377d07e22e882209
ben <zhaobinxian@greatyun.com>
2021-09-07 11:18:09 +0800

    change gradle to maven for devtools

---
+++ spring-boot-project/spring-boot-devtools/src/main/java/org/springframework/boot/devtools/SpringBootDevtoolsApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.devtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDevtoolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDevtoolsApplication.class, args);
+    }
+
+}
