03adaa5cc7cf11c62dcccd499f65c31dd665b034
ben <zhaobinxian@greatyun.com>
2021-09-07 10:56:08 +0800

    change gradle to maven for actuator-autoconfigure

---
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/main/java/org/springframework/boot/actuate/autoconfigure/SpringBootActuatorAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.actuate.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootActuatorAutoconfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorAutoconfigureApplication.class, args);
+    }
+
+}
---
+++ spring-boot-project/spring-boot-actuator-autoconfigure/src/test/java/org/springframework/boot/actuate/autoconfigure/SpringBootActuatorAutoconfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.actuate.autoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorAutoconfigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
