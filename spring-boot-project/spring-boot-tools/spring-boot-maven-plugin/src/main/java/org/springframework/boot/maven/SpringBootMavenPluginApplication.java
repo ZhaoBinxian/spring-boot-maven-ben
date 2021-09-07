5a1b4208eb708299cbac3514637eb855a78cd186
ben <zhaobinxian@greatyun.com>
2021-09-07 12:55:32 +0800

    change gradle to maven for maven-plugin

---
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/main/java/org/springframework/boot/maven/SpringBootMavenPluginApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenPluginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenPluginApplication.class, args);
+    }
+
+}
---
+++ spring-boot-project/spring-boot-tools/spring-boot-maven-plugin/src/test/java/org/springframework/boot/maven/SpringBootMavenPluginApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.maven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMavenPluginApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
