d9320c278957e5f0ad43d49e3d6582df187f5294
ben <zhaobinxian@greatyun.com>
2021-09-07 11:21:43 +0800

    change gradle to maven for docs

---
+++ spring-boot-project/spring-boot-docs/src/main/java/org/springframework/boot/docs/SpringBootDocsApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.docs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDocsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-project/spring-boot-docs/src/test/java/org/springframework/boot/docs/SpringBootDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.docs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
