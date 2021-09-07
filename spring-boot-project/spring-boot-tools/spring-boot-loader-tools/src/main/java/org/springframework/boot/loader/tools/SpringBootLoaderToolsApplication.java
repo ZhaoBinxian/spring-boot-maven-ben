b5c0f8f2051b5b7e3385042e2ebf90d5874cc9b5
ben <zhaobinxian@greatyun.com>
2021-09-07 11:42:54 +0800

    change gradle to maven for loader-tools

---
+++ spring-boot-project/spring-boot-tools/spring-boot-loader-tools/src/main/java/org/springframework/boot/loader/tools/SpringBootLoaderToolsApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.loader.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLoaderToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLoaderToolsApplication.class, args);
+    }
+
+}
---
+++ spring-boot-project/spring-boot-tools/spring-boot-loader-tools/src/test/java/org/springframework/boot/loader/tools/SpringBootLoaderToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.loader.tools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLoaderToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
