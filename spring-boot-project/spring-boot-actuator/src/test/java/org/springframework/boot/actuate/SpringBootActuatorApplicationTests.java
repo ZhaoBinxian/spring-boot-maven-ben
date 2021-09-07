d69aeee8ef37f368936292a00af6a3d4fd7a3c26
ben <zhaobinxian@greatyun.com>
2021-09-07 10:54:28 +0800

    change gradle to maven for actuator

---
+++ spring-boot-project/spring-boot-actuator/src/test/java/org/springframework/boot/actuate/SpringBootActuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.actuate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootActuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
