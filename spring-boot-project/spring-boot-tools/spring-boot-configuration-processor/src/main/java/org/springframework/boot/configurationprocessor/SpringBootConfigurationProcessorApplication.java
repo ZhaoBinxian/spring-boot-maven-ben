26a8fefa5cf2c41b63be416ef0d068a2e367d4f6
ben <zhaobinxian@greatyun.com>
2021-09-07 11:16:50 +0800

    change gradle to maven for configuration-processor

---
+++ spring-boot-project/spring-boot-tools/spring-boot-configuration-processor/src/main/java/org/springframework/boot/configurationprocessor/SpringBootConfigurationProcessorApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.configurationprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootConfigurationProcessorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootConfigurationProcessorApplication.class, args);
+    }
+
+}
