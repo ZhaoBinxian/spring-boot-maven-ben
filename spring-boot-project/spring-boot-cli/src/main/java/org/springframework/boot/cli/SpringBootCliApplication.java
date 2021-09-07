28f82efbfa0b690aac9d1dae8a0feb698c2e44cf
ben <zhaobinxian@greatyun.com>
2021-09-07 11:10:49 +0800

    change gradle to maven for cli

---
+++ spring-boot-project/spring-boot-cli/src/main/java/org/springframework/boot/cli/SpringBootCliApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCliApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootCliApplication.class, args);
+    }
+
+}
