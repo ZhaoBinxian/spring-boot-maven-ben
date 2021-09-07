3122a6a3fd9a3b89c41f970d01bb82effa2a4fc8
ben <zhaobinxian@greatyun.com>
2021-09-07 11:04:04 +0800

    change gradle to maven for autoconfigure

---
+++ spring-boot-project/spring-boot-autoconfigure/src/main/java/org/springframework/boot/autoconfigure/SpringBootAutoconfigureApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.autoconfigure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAutoconfigureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAutoconfigureApplication.class, args);
+    }
+
+}
---
+++ spring-boot-project/spring-boot-autoconfigure/src/test/java/org/springframework/boot/autoconfigure/SpringBootAutoconfigureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.autoconfigure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAutoconfigureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
